41e218e6d4c3fc977b4fd3c51b60e44fdcfb0961
DESKTOP-FEDOR <pichuginf2016@mail.ru>
2023-10-25 19:04:36 +0500

    da

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
 using Game.Domain;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
 using WebApi.Models;
 
 namespace WebApi.Controllers
@@ -9,21 +15,162 @@ namespace WebApi.Controllers
     [ApiController]
     public class UsersController : Controller
     {
+        private readonly IUserRepository userRepository;
+        private readonly IMapper mapper;
+        private readonly LinkGenerator linkGenerator;
+
         // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-        public UsersController(IUserRepository userRepository)
+        public UsersController(IUserRepository userRepository, IMapper mapper, LinkGenerator linkGenerator)
         {
+            this.userRepository = userRepository;
+            this.mapper = mapper;
+            this.linkGenerator = linkGenerator;
         }
 
-        [HttpGet("{userId}")]
+
+        [HttpGet("{userId}", Name = nameof(GetUserById))]
+        [HttpHead("{userId}")]
+        [Produces("application/json", "application/xml")]
         public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
         {
-            throw new NotImplementedException();
+            var user = userRepository.FindById(userId);
+            if (user is null)
+            {
+                return NotFound();
+            }
+
+            var userDto = mapper.Map<UserDto>(user);
+
+            return Ok(userDto);
         }
 
         [HttpPost]
-        public IActionResult CreateUser([FromBody] object user)
+        [Produces("application/json", "application/xml")]
+        public IActionResult CreateUser([FromBody] PostUserDto postUser)
         {
-            throw new NotImplementedException();
+            if (postUser is null)
+                return BadRequest();
+
+            if (postUser.Login is null)
+                ModelState.AddModelError("login", "Login must not be null");
+
+            else if (postUser.Login.Any(ch => !char.IsLetterOrDigit(ch)))
+                ModelState.AddModelError("login", "Login must contains only letters or digits");
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
+            var userEntity = mapper.Map<UserEntity>(postUser);
+            var createdUserEntity = userRepository.Insert(userEntity);
+            return CreatedAtRoute(
+                nameof(GetUserById),
+                new { userId = createdUserEntity.Id },
+                createdUserEntity.Id);
+        }
+
+        [HttpPut("{userId}")]
+        [Produces("application/json", "application/xml")]
+        public IActionResult UpdateUser([FromBody] PutUserDto user, Guid userId)
+        {
+            if (user is null || userId == Guid.Empty)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
+            var userEntityWithId = new UserEntity(id: userId);
+            var userEntity = mapper.Map(user, userEntityWithId);
+            userRepository.UpdateOrInsert(userEntity, isInserted: out var isInserted);
+
+            if (isInserted)
+            {
+                return CreatedAtRoute(
+                    nameof(GetUserById),
+                    new { userId = userEntity.Id },
+                    userEntity.Id);
+            }
+
+            return NoContent();
+        }
+
+        [HttpPatch("{userId}")]
+        [Produces("application/json", "application/xml")]
+        public IActionResult PartiallyUpdateUser([FromBody] JsonPatchDocument<PatchUserDto> patchDoc, Guid userId)
+        {
+            if (patchDoc is null)
+                return BadRequest();
+
+            if (userId == Guid.Empty)
+                return NotFound();
+
+            var user = userRepository.FindById(userId);
+
+            if (user is null)
+                return NotFound();
+
+            var patchUserDto = new PatchUserDto();
+            mapper.Map(user, patchUserDto);
+            patchDoc.ApplyTo(patchUserDto, ModelState);
+
+            TryValidateModel(patchUserDto);
+
+            return ModelState.IsValid ? NoContent() : UnprocessableEntity(ModelState);
+        }
+
+
+        [HttpDelete("{userId}")]
+        [Produces("application/json", "application/html")]
+        public IActionResult DeleteUser(Guid userId)
+        {
+            if (userId == Guid.Empty)
+                return NotFound();
+
+            var user = userRepository.FindById(userId);
+
+            if (user is null)
+                return NotFound();
+
+            userRepository.Delete(userId);
+            return NoContent();
+        }
+
+
+        [HttpGet(Name = nameof(GetUsers))]
+        [Produces("application/json", "application/html")]
+        public IActionResult GetUsers([FromQuery] int? pageNumber, [FromQuery] int? pageSize)
+        {
+            var intPageNumber = pageNumber == null ? 1 : Math.Max(1, pageNumber.Value);
+            var intPageSize = pageSize == null ? 10 : Math.Min(20, Math.Max(1, pageSize.Value));
+
+            var pageList = userRepository.GetPage(intPageNumber, intPageSize);
+
+            var paginationHeader = new
+            {
+                previousPageLink = pageList.HasPrevious
+                    ? linkGenerator.GetUriByRouteValues(HttpContext, nameof(GetUsers),
+                        new { pageNuber = pageList.CurrentPage - 1, intPageSize })
+                    : null,
+                nextPageLink = pageList.HasNext
+                    ? linkGenerator.GetUriByRouteValues(HttpContext, nameof(GetUsers),
+                        new { pageNuber = pageList.CurrentPage + 1, intPageSize })
+                    : null,
+                totalCount = pageList.TotalCount,
+                pageSize = pageList.PageSize,
+                currentPage = pageList.CurrentPage,
+                totalPages = pageList.TotalPages,
+            };
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(paginationHeader));
+
+            var users = mapper.Map<IEnumerable<UserDto>>(pageList);
+            return Ok(users);
+        }
+
+        [HttpOptions]
+        [Produces("application/json", "application/html")]
+        public IActionResult UserOptions()
+        {
+            Response.Headers.Add("Allow", "GET, POST, OPTIONS");
+            return Ok();
         }
     }
 }
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,7 +1,13 @@
+using AutoMapper;
+using Game.Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using WebApi.Models;
 
 namespace WebApi
 {
@@ -17,11 +23,36 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers()
-                .ConfigureApiBehaviorOptions(options => {
+            services.AddAutoMapper(cfg =>
+                {
+                    cfg.CreateMap<UserEntity, UserDto>()
+                        .ForMember(dest => dest.FullName, opt =>
+                            opt.MapFrom(src => $"{src.LastName} {src.FirstName}"));
+
+                    cfg.CreateMap<PostUserDto, UserEntity>();
+                    cfg.CreateMap<PutUserDto, UserEntity>();
+                    cfg.CreateMap<PatchUserDto, UserEntity>();
+                    cfg.CreateMap<UserEntity, PatchUserDto>();
+                },
+                new System.Reflection.Assembly[0]);
+
+            services.AddControllers(options =>
+                {
+                    // Этот OutputFormatter позволяет возвращать данные в XML, если требуется.
+                    options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                    // Эта настройка позволяет отвечать кодом 406 Not Acceptable на запросы неизвестных форматов.
+                    options.ReturnHttpNotAcceptable = true;
+                    // Эта настройка приводит к игнорированию заголовка Accept, когда он содержит */*
+                    // Здесь она нужна, чтобы в этом случае ответ возвращался в формате JSON
+                    options.RespectBrowserAcceptHeader = true;
+                })
+                .ConfigureApiBehaviorOptions(options =>
+                {
                     options.SuppressModelStateInvalidFilter = true;
                     options.SuppressMapClientErrors = true;
-                });
+                })
+                .AddNewtonsoftJson(options => { options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver(); });
+            services.AddSingleton<IUserRepository, InMemoryUserRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,10 +64,7 @@ namespace WebApi
 
             app.UseRouting();
             app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
